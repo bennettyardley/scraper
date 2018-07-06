46e3cfb4fca823dd9a8a9e71d7e22ae52b9d187f
Bennett Yardley <33298178+bennettyardley@users.noreply.github.com>
2018-07-05 21:02:17 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import time
 import urllib.request
+from urllib.parse import urlparse
+import pickle
+import os
 
 js = 'C:\\chromedriver.exe'
 driver = webdriver.Chrome(js)
@@ -36,3 +39,57 @@ for a in links:
         linky = divy.findAll('a')
         for lunk in linky:
             print(a['href'])
+
+hunks = []
+newLunks = []
+
+soup = BeautifulSoup(open("C:\\somthing.html", encoding="utf8"), "html.parser")
+data = soup.findAll('div',attrs={'class':'business-attribution__business-info'})
+for div in data:
+    blinks = div.findAll('a')
+    for a in blinks:
+        hunks.append(a['href'])
+countdown = len(hunks)
+
+for link in hunks:
+    try:
+        print(countdown)
+        request = urllib.request.urlopen(link)
+        soupy = BeautifulSoup(request, 'lxml')
+        datay = soupy.findAll('div',attrs={'class':'u--td profile-info__item'})
+        for divy in datay:
+            linky = divy.findAll('a')
+            for a in linky:
+                newLunks.append(a['href'])
+        countdown = countdown - 1
+    except:
+        countdown = countdown - 1
+        pass
+
+with open('testfile2.txt', 'wb') as fp:
+    pickle.dump(newLunks, fp)
+
+with open ('testfile2.txt', 'rb') as fp:
+    itemlist = pickle.load(fp)
+newlist = []
+for item in itemlist:
+    if "tel:" in item:
+        pass
+    elif "face" in item:
+        pass
+    else:
+        newlist.append(item)
+
+with open('testfile3.txt', 'wb') as fp:
+    pickle.dump(newlist, fp)
+
+with open ('testfile3.txt', 'rb') as fp:
+    weblist = pickle.load(fp)
+
+for website in weblist:
+    parsed_url = urlparse(website)
+    domain = '{uri.scheme}://{uri.netloc}'.format(uri=parsed_url)
+    webaddr = '{uri.netloc}'.format(uri=parsed_url)
+    print(domain)
+    print(webaddr)
+    #os.system(r"C:\wget.exe --recursive --no-clobber --convert-links --adjust-extension --domains " + domain + " " + website)
